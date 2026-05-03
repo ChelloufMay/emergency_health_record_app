4b6f40abbd93e1e02283b075b01b214916be560c
ChelloufMay <maychellouf28@gmail.com>
2026-05-03 12:13:47 +0100

    update main for the new files

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'config/supabase_config.dart';
+import 'screens/allergies_screen.dart';
+import 'screens/attachments_screen.dart';
 import 'screens/audit_log_screen.dart';
 import 'screens/caregiver_screen.dart';
+import 'screens/conditions_screen.dart';
 import 'screens/emergency_screen.dart';
+import 'screens/family_doctor_screen.dart';
+import 'screens/family_history_screen.dart';
 import 'screens/home_screen.dart';
+import 'screens/hospitalizations_screen.dart';
+import 'screens/lifestyle_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/medical_summary_screen.dart';
+import 'screens/medications_screen.dart';
 import 'screens/profile_screen.dart';
 import 'screens/qr_screen.dart';
 import 'screens/register_screen.dart';
+import 'screens/reproductive_health_screen.dart';
 import 'screens/settings_screen.dart';
+import 'screens/surgeries_screen.dart';
+import 'screens/vaccinations_screen.dart';
 import 'screens/welcome_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Supabase.initialize(
     url: SupabaseConfig.url,
     anonKey: SupabaseConfig.anonKey,
   );
-
   runApp(const MyApp());
 }
 
@@ -52,6 +61,17 @@ class MyApp extends StatelessWidget {
         '/caregivers': (context) => const CaregiverScreen(),
         '/audit_log': (context) => const AuditLogScreen(),
         '/settings': (context) => const SettingsScreen(),
+        '/allergies': (context) => const AllergiesScreen(),
+        '/medications': (context) => const MedicationsScreen(),
+        '/conditions': (context) => const ConditionsScreen(),
+        '/surgeries': (context) => const SurgeriesScreen(),
+        '/hospitalizations': (context) => const HospitalizationsScreen(),
+        '/vaccinations': (context) => const VaccinationsScreen(),
+        '/lifestyle': (context) => const LifestyleScreen(),
+        '/family_history': (context) => const FamilyHistoryScreen(),
+        '/reproductive_health': (context) => const ReproductiveHealthScreen(),
+        '/family_doctor': (context) => const FamilyDoctorScreen(),
+        '/attachments': (context) => const AttachmentsScreen(),
       },
     );
   }
